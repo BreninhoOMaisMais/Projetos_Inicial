fbba4aba357f3ad9f2d837d5abfb67c6cb8a7da1
BreninhoOMaisMais <breninff10@gmail.com>
2024-03-18 09:56:11 -0300

    Formulario

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);  
-
 var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var num = document.getElementById("num");
 
 function alertar(){
     alert(nome.value + " " + "clicou no botão!!!");
+    saida.innerText = "Nome: " + " " + nome.value +
+    "\n E-mail: " + " " + email.value +  "\n Telefone: " + " "
+    + telefone.value + "\n CEP: " + " " + cep.value +
+    "\n Logradouro: " + " " + log.value+
+    "\n Número: " + " " + num.value ;
 }
