16d5d5ff1b8e141ab97a2de1dd75ecbbf39e50ea
BreninhoOMaisMais <breninff10@gmail.com>
2024-03-19 09:15:22 -0300

    busca cep

--- scripts.js
+++ scripts.js
@@ -8,10 +8,29 @@ var cep = document.getElementById("cep");
 var num = document.getElementById("num");
 
 function alertar(){
-    alert(nome.value + " " + "clicou no botão!!!");
-    saida.innerText = "Nome: " + " " + nome.value +
-    "\n E-mail: " + " " + email.value +  "\n Telefone: " + " "
-    + telefone.value + "\n CEP: " + " " + cep.value +
-    "\n Logradouro: " + " " + log.value+
-    "\n Número: " + " " + num.value ;
+
+    // buscar o endereço pelo cep
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+
+    fetch(url) 
+      .then(resposta=>resposta.json())
+      .then(data => {
+            logradouro.value = data.logradouro;
+            bairro.value = data.bairro;
+            cidade.value = data.cidade;
+            estado.value = data.uf;
+
+            alert(nome.value + " " + "clicou no botão!!!");
+            saida.innerText = 
+            "Nome: " + " " + nome.value +
+            "\n E-mail: " + " " + email.value + 
+            "\n Telefone: " + " "+ telefone.value +
+            "\n CEP: " + " " + cep.value +
+            "\n Logradouro: " + " " + log.value+
+            "\n Número: " + " " + num.value ;
+
+    })
+    .catch(error=>alert(error))
+
+
 }
